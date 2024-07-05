1894fed67537ec93adc1e1170c51d534c0c4d27c
Comfort Ngwenya <>
2024-07-05 02:03:52 +0200

    styling

--- wudo/src/components/left/left.jsx
+++ wudo/src/components/left/left.jsx
@@ -4,31 +4,36 @@ import RadioButtonCheckedOutlinedIcon from '@mui/icons-material/RadioButtonCheck
 export default function left() {
   return (
     <div className="left">
+
       <div className="leftTop">
         <img src = "./assets/images/drop-cishio-chair.jpg" alt=""className="leftTopImg"></img>
       </div>
+
       <div className="middle">
 
-      <div class="line-container">
-        <div class="horizontal-line"></div>
-        <div class="button-container">
-        <RadioButtonCheckedOutlinedIcon className='radioBtnGrey' />
+        <div class="line-container">
+
+          <div class="horizontal-line"></div>
+
+          <div class="button-container">
+            <RadioButtonCheckedOutlinedIcon className='radioBtnGrey' />
+            <p className='rotate'>Rotate</p>
+          </div>
         </div>
-    </div>
 
       </div>
       <div className="leftBottom">
         <div className="bottomBoxes">
           <div className="bottomBox">
-            <img src = "./assets/images/chair-3.jpg" alt='' className="bottomImg" />
+            <img src = "./assets/images/chair1.jpg" alt='' className="bottomImg" />
           </div>
           
           <div className="bottomBox">
-            <img src = "./assets/images/chair-1.jpg" alt='' className="bottomImg" />
+            <img src = "./assets/images/chair2.jpg" alt='' className="bottomImg" />
           </div>
 
           <div className="bottomBox">
-            <img src = "./assets/images/chair-2.jpg" alt='' className="bottomImg" />
+            <img src = "./assets/images/chair3.jpg" alt='' className="bottomImg" />
           </div>
           
           
--- wudo/src/components/right/right.jsx
+++ wudo/src/components/right/right.jsx
@@ -6,57 +6,60 @@ import RadioButtonCheckedOutlinedIcon from '@mui/icons-material/RadioButtonCheck
 export default function right() {
   return (
     <div className="right">
-      <p className='right-text'>PREMIUM CHAIRS</p>
-      <h2>Drop type cusion chair</h2>
+      <div className="rightContainer">
+        <p className='right-text'>PREMIUM CHAIRS</p>
+        <h2>Drop type cusion chair</h2>
 
-      <div className="reviews">
-        <StarOutlinedIcon className='star'/>
-        <label className='review'>
+        <div className="reviews">
+          <StarOutlinedIcon className='star'/>
+          <label className='review'>
           <strong>8/10.  </strong> 
           15 Rewiews 
-        </label>
-      </div>
+          </label>
+        </div>
 
-      <div className="radioButtons">
-        <RadioButtonCheckedOutlinedIcon className='radioBtnGrey' />
-        <RadioButtonCheckedOutlinedIcon className='radioBtnBlue'/>
-      </div>
+        <div className="radioButtons">
+          <RadioButtonCheckedOutlinedIcon className='radioBtnGrey' />
+          <RadioButtonCheckedOutlinedIcon className='radioBtnBlue'/>
+        </div>
 
 
-      <div className="productInfo">
+        <div className="productInfo">
 
-        <p>Premium & comfortable memory foam with
-          a strong structure built with teak wood,
-          it feels amazing.
-        </p>
+          <p>Premium & comfortable memory foam with
+            a strong structure built with teak wood,
+            it feels amazing.
+          </p>
 
-        <div className="productTable">
-          <table>
-            <tr>
-              <td>HEIGHT <br></br><span><strong>52 </strong></span>cm</td>
-              <td>WIDTH <br></br> <span><strong>43 </strong></span>cm</td>
-            </tr>
-          </table>
+          <div className="productTable">
+            <table>
+              <tr>
+                <td>HEIGHT <br></br><span><strong>52 </strong></span>cm</td>
+                <td>WIDTH <br></br> <span><strong>43 </strong></span>cm</td>
+              </tr>
+            </table>
 
-          <p className='inches'>view in inches</p>
+            <p className='inches'>view in inches</p>
 
+            
+          </div>
+
+          <p className='price'><strong>$265.50</strong></p>
+
+          <div className="quantityAdd">
+            <button className='remove'> - </button>
+            <p className='cartNum'> <b>1</b> </p>
+            <button className='add'> + </button>
+          </div>
+
+          <div className="cartBuyBtn">
+            <button className='addToCart'>ADD TO CART</button>
+            <button className='buyNow'>BUY NOW</button>
+          </div>
           
         </div>
-
-        <p className='price'><strong>$265.50</strong></p>
-
-        <div className="quantityAdd">
-          <button className='remove'> - </button>
-          <p className='cartNum'> 1 </p>
-          <button className='add'> + </button>
-        </div>
-
-        <div className="cartBuyBtn">
-          <button className='addToCart'>ADD TO CART</button>
-          <button className='buyNow'>BUY NOW</button>
-        </div>
-        
       </div>
+      
       
     </div>
   )
